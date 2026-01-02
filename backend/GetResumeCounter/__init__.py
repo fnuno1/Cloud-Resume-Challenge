faec0760d5d5dd04e38eea6181d9b7cc5a370cca
fnuno1 <fnuno1@wgu.edu>
2026-01-02 00:48:13 -0600

    Add backend folder with starter Azure Function files

---
+++ backend/GetResumeCounter/__init__.py
@@ -0,0 +1,8 @@
+﻿import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    # TODO: Connect to Cosmos DB and increment visitor count
+    return func.HttpResponse(
+        "Visitor counter function placeholder",
+        status_code=200
+    )
